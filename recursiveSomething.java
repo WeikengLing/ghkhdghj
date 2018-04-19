db25efa989ecd9e14a8539b05c6fc3c46ae47b16
WeikengLing <31445484+WeikengLing@users.noreply.github.com>
2018-04-19 10:30:41 -0500

    fch,gd,fjh

--- recursiveSomething.java
+++ recursiveSomething.java
@@ -1,7 +1,7 @@
 import javax.swing.JApplet;
 import java.awt.*;
 
-public class recursiveTriangle18 extends JApplet
+public class recursiveSomething extends JApplet
 {
     private final int APPLET_WIDTH = 800;
     private final int APPLET_HEIGHT = 800;
@@ -13,8 +13,8 @@ public class recursiveTriangle18 extends JApplet
     point 3 - Top	C x[2],y[2]
     point 4 draws back to point 1 to complete triangle
     */
-    private int[] xPos = {720, 80, 400, 720};
-    private int[] yPos = {600, 600, 40, 600};
+    private int[] xPos = {720, 80, 80, 720, 720};
+    private int[] yPos = {680, 680, 40, 40, 680};
     
     //-----------------------------------------------------------------
     //  Sets up the basic applet environment.
@@ -30,15 +30,12 @@ public class recursiveTriangle18 extends JApplet
     //-----------------------------------------------------------------
     public void paint (Graphics page)
     {
-    
         page.setColor (Color.red);
         page.drawPolyline (xPos, yPos, xPos.length);
-        
-        Triangle(xPos,yPos,page);
-        
+        Square(xPos,yPos,page);
     }//end of paint
     
-    public void Triangle(int[] xPos, int[] yPos, Graphics page)
+    public void Square(int[] xPos, int[] yPos, Graphics page)
     {
         //Find the distance between 2 points ex. - x,y & x1,y1
         int length = xPos[0] - xPos[1];
@@ -47,31 +44,33 @@ public class recursiveTriangle18 extends JApplet
         if (length > 30)
         {
             //find the mid points of each line segment
-            int xrm = (xPos[0] - xPos[2])/2 + xPos[2];
-            int xlm = (xPos[2] - xPos[1])/2 + xPos[1];
-            int xbm = xPos[2];
-            int ytm = (yPos[0] - yPos[2])/2 + yPos[2];
-            int ybm = yPos[0];
+            int xrm = xPos[0];
+            int xbm = xPos[1] + length / 2;
+            int xlm = xPos[1];
+            int xtm = xPos[1] + length / 2;
+            int ytop = yPos[2];
+            int ym = yPos[2] + length / 2;
+            int ybot = yPos[0];
+            int l4 = length / 4;
             
             //make the x and y array (3 points + first point to finish triangle)
-            int[] xnew = {xrm, xlm, xbm, xrm};
-            int[] ynew = {ytm, ytm, ybm, ytm};
+            int[] xnew = {xrm, xbm, xlm, xtm, xrm};
+            int[] ynew = {ym, ybot, ym, ytop, ym};
             
             //draw the Triangle
             page.drawPolyline (xnew, ynew, xnew.length);
             
             //create x,y Array using the midpoints you calculated
-            int[] tx = {xrm, xlm, xPos[2], xrm};
-            int[] ty = {ytm, ytm, yPos[2], ytm};
-            int[] rx = {xPos[0], xbm, xrm, xPos[0]};
-            int[] ry = {ybm, ybm, ytm, ybm};
-            int[] lx = {xbm, xPos[1], xlm, xbm};
-            int[] ly = {ybm, ybm, ytm, ybm};
+            int[] xr = {xPos[0], xPos[0] - l4, xPos[0] - l4, xPos[0], xPos[0]};
+            int[] xl = {xPos[1] + l4, xPos[1], xPos[1], xPos[1] + l4, xPos[1] + l4};
+            int[] yt = {yPos[2] + l4, yPos[2] + l4, yPos[2], yPos[2], yPos[2] + l4};
+            int[] yb = {yPos[0], yPos[0], yPos[0] - l4, yPos[0] - l4, yPos[0]};
             
             // Recursive calls for each section of triangle
-            Triangle(tx, ty, page);
-            Triangle(rx, ry, page);
-            Triangle(lx, ly, page);
+            Square(xr, yt, page);
+            Square(xr, yb, page);
+            Square(xl, yt, page);
+            Square(xl, yb, page);
         }
     }//end of Triangle
 }
